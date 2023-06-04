f20539ac3f28fdd313db81d8ddaab08186edf49b
sandipKhatana2004 <sandipkhatana2004@gmail.com>
2023-06-04 11:11:26 +0530

    Authentication Issue Resolved.

---
+++ src/components/NewTweetForm.tsx
@@ -0,0 +1,6 @@
+export function NewTweetForm() {
+    return <form className="flex flex-col gap-2 border-b px-44 py-2">
+        <div>sandip Khatana</div>
+        <button>Add</button>
+    </form>
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import { type NextPage } from "next";
+import { NewTweetForm } from "~/components/NewTweetForm";
+
+const Home: NextPage = () => {
+
+  return <>
+  <header className="sticky top-0 z-10 border-b bg-white pt-2">
+    <h1 className="mb-2 px-4 text-lg font-bold">
+      Home
+    </h1>
+  </header>
+  <NewTweetForm />
+  </>
+};
+
+export default Home
