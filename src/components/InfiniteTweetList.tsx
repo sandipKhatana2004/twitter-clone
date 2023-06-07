7359353f844243e2aa9a091a69e12f293f310ae1
sandipKhatana2004 <sandipkhatana2004@gmail.com>
2023-06-07 18:09:02 +0530

    Designed some cool effects and refined tweet system by adding TweetCart.

--- src/components/InfiniteTweetList.tsx
+++ src/components/InfiniteTweetList.tsx
@@ -1,5 +1,9 @@
+import Link from "next/link";
 import InfiniteScroll from "react-infinite-scroll-component"
-
+import { ProfileImage } from "./ProfileImage";
+import { useSession } from "next-auth/react";
+import {VscHeart, VscHeartFilled} from "react-icons/vsc"
+import { IconHoverEffect } from "./IconHoverEffect";
 
 type Tweet = {
   id: string;
@@ -33,11 +37,61 @@ export function InfiniteTweetList({tweets, isError, isLoading, fetchNewTweets, h
              next={fetchNewTweets}
              hasMore={hasMore}
              loader={"Loading..."}>
-                {tweets.map(tweet => {
-                    return <div key={tweet.id}>{tweet.content}</div>
-                }) } 
+                {tweets.map((tweet) => {
+                    return <TweetCard key={tweet.id} {...tweet} />
+                })}
             </InfiniteScroll>
         </ul>
     )
 
+}
+
+const dateTimeFormatter = Intl.DateTimeFormat(undefined, { dateStyle: "short"})
+
+function TweetCard({id, user, content, createdAt, likeCount, likedByMe}: Tweet) {
+    return( 
+    <li className="flex gap-4 border-b py-4 px-4">
+        <Link href={`/profile/${user.id}`}>
+        <ProfileImage src={user.image} /> 
+        </Link>
+        <div className="flex flex-grow flex-col">
+            <div className="flex gap-1">
+                <Link href={`/profile/${user.id}`}
+                className="font-bold hover:underline focus-visible:underline"
+                >
+                    {user.name}
+                </Link>
+                <span className="text-gray-500"></span>
+                <span className="text-gray-500">{dateTimeFormatter.format()}</span>
+            </div>
+            <p className="whitespace-pre-wrap">{content}</p>
+            <HeartButton likedByMe={likedByMe} likeCount={likeCount} />
+        </div>
+    </li>
+    )
+}
+type HeartButtonProps = {
+    likedByMe: boolean
+    likeCount: number
+}
+
+
+function HeartButton({likedByMe, likeCount} : HeartButtonProps) {
+    const session = useSession()
+    const HeartIcon = likedByMe ? VscHeartFilled : VscHeart;
+
+    if(session.status !== "authenticated") {
+        return <div className="mb-1 mt-1 flex items-center gap-3 self-start text-gray-500">
+            <HeartIcon />
+            <span>{likeCount}</span>
+        </div>
+    }
+    return (
+        <button className={`group flex items-center gap-1 self-start  transition-colors duration-200 -ml-2 ${likedByMe ? "text-red-500" : "text-gray-500 hover:text-red-500 focus-visible:text-red-500"}`}>
+            <IconHoverEffect>
+            <HeartIcon className={`transition-colors duration-200 ${likedByMe ? "fill-red-500" : "fill-gray-500 group-hover:fill-red-500 group-focus-visible:fill-red-500"}`}/>
+            </IconHoverEffect>
+            <span>{likeCount}</span>
+        </button>
+    )
 }
